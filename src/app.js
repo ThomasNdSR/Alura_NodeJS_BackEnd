baaf0b6b28fe45c704218f8b2ac13112c4603ab4
Thomas Rodrigues <thomasnicola@outlook.com>
2022-04-04 11:44:17 -0300

    Projeto livraria

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json()); //recurso para interpretar o que ta chegando via post/put como um objeto para podermos manipular
+
 const livros = [
-    {id: 1, "titulo": "Senhor dos Aneis"},
-    {id: 2, "titulo": "O Hobbit"}
+    { id: 1, "titulo": "Senhor dos Aneis" },
+    { id: 2, "titulo": "O Hobbit" }
 ]
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.status(200).send('Curso de Node');
 })
+
 app.get('/livros', (req, res) => {
     res.status(200).json(livros);
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body); /*adicionar o conteúdo do corpo da requisição no array livros.*/
+    res.status(201).send('Livro foi cadastrado com sucesso');
 })
 
 export default app;
