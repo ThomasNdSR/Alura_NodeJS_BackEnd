6f2b22ff3a22258ab70aa4a58b3ce93d51252ca0
Thomas Rodrigues <thomasnicola@outlook.com>
2022-04-04 12:15:22 -0300

    Evoluindo as rotas e finalizando o CRUD

--- src/app.js
+++ src/app.js
@@ -17,9 +17,31 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 })
 
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id); //id enviado no endpoint
+    res.json(livros[index]);
+})
+
 app.post('/livros', (req, res) => {
     livros.push(req.body); /*adicionar o conteúdo do corpo da requisição no array livros.*/
     res.status(201).send('Livro foi cadastrado com sucesso');
 })
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo; /*titulo enviado pelo corpo da requisição */
+    res.json(livros);
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1)
+    res.send('Livro removido com sucesso')
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id);
+}
 
 export default app;
