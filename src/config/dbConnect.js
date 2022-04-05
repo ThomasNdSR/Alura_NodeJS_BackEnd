39092fb88f2e188e36222c13e53a26bde51bee3f
Thomas Rodrigues <thomasnicola@outlook.com>
2022-04-05 09:59:19 -0300

    "Iniciando com Mongoose"

---
+++ src/config/dbConnect.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://thmsnsr:g9S68XeScGvyh..@cluster0.vkmf7.mongodb.net/alura-node?");
+
+let db = mongoose.connection;
+
+export default db;
