225a865e0bd447174242227db7ba837ad18b2621
Thomas Rodrigues <thomasnicola@outlook.com>
2022-04-21 17:32:41 -0300

    "Associando dados entre Schemas"

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,22 +2,28 @@ import livros from "../models/Livro.js";
 
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
-      res.status(200).json(livros);
-    });
+    livros
+      .find()
+      .populate("autor")
+      .exec((err, livros) => {
+        res.status(200).json(livros);
+      });
   };
 
   static listarLivroPorId = (req, res) => {
     const id = req.params.id;
-    livros.findById(id, (err, livros) => {
-      if (err) {
-        res
-          .status(400)
-          .send({ message: `${err.message} - Id do livro não localizado.` });
-      } else {
-        res.status(200).send(livros);
-      }
-    });
+    livros
+      .findById(id)
+      .populate("autor", "nome")
+      .exec((err, livros) => {
+        if (err) {
+          res
+            .status(400)
+            .send({ message: `${err.message} - Id do livro não localizado.` });
+        } else {
+          res.status(200).send(livros);
+        }
+      });
   };
 
   static cadastrarLivro = (req, res) => {
@@ -48,11 +54,11 @@ class LivroController {
   static excluirLivro = (req, res) => {
     const id = req.params.id;
     livros.findByIdAndDelete(id, (err) => {
-        if (!err) {
-            res.status(200).send({message: "Livro removido com sucesso."})
-        } else {
-            res.status(500).send({message: err.message})
-        }
+      if (!err) {
+        res.status(200).send({ message: "Livro removido com sucesso." });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
     });
   };
 }
